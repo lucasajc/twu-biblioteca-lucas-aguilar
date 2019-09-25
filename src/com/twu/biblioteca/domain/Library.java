7e26b1fac454e4c68c31090efe9b9fb83a03be9a
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2019-09-25 16:27:49 -0300

    [1.2/3] Add library domain implementation

---
+++ src/com/twu/biblioteca/domain/Library.java
@@ -0,0 +1,36 @@
+package com.twu.biblioteca.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Library {
+
+    private HashMap<UUID, Book> books;
+    private static final String LIST_FORMAT = "%-25s%25s%25s%25s%n";
+    private static final String[] LIST_HEADER = new String[] { "Id", "Author", "Title", "Year published" };
+
+    public Library(ArrayList<Book> bookList) {
+        this.books = new HashMap<UUID, Book>();
+        for (Book book : bookList) {
+            this.books.put(book.getId(), book);
+        }
+    }
+
+    public void list() {
+        System.out.println("\nList of books:\n");
+        System.out.format(LIST_FORMAT, LIST_HEADER);
+
+        for(Book book : this.books.values()) {
+            System.out.format(LIST_FORMAT,
+                    book.getId(),
+                    book.getAuthor(),
+                    book.getTitle(),
+                    book.getYear());
+        }
+    }
+
+    public HashMap<UUID, Book> getBooks() {
+        return books;
+    }
+}
